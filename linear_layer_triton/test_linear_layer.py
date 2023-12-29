917d1cca6617fbbcdfdfd573e6ebcce2b50f921e
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2023-12-29 02:43:48 -0800

    Update testing code with modified API

--- linear_layer_triton/test_linear_layer.py
+++ linear_layer_triton/test_linear_layer.py
@@ -1,5 +1,5 @@
 import torch
-from linear_layer import linear_layer
+from linear_layer import linear_layer_triton
 import time
 
 torch.manual_seed(0)
@@ -29,19 +29,31 @@ bias = ll_layer.bias
 
 for i in range(10):
     _ = act(ll_layer(x))
-    _ = linear_layer(x, weight, bias, add_bias=ADD_BIAS)
-    _ = linear_layer(x, weight, bias, activation=ACTIVATION, add_bias=ADD_BIAS)
+    _ = linear_layer_triton(x, weight, bias)
+    _ = linear_layer_triton(x, weight, bias, activation=ACTIVATION)
 
 print("Warmup (for Torch) & Compilation (for Triton) done!")
 
-t1 = time.time_ns()
-triton_output = linear_layer(x, weight, bias, activation=ACTIVATION, add_bias=ADD_BIAS)
-t2 = time.time_ns()
-torch_output = act(ll_layer(x))
-t3 = time.time_ns()
+triton_time = 0
+for i in range(10):
+    t1 = time.time_ns()
+    triton_output = linear_layer_triton(x, weight, bias, activation=ACTIVATION)
+    t2 = time.time_ns()
+    triton_time += (t2 - t1)
 
-print(f"Triton time: {(t2 - t1)/1000000}ms")
-print(f"Torch time: {(t3 - t2)/1000000}ms")
+triton_time = triton_time / 10 / 1_000_000   
+
+torch_time = 0
+for i in range(10):
+    t1 = time.time_ns()
+    torch_output = act(ll_layer(x))
+    t2 = time.time_ns()
+    torch_time += (t2 - t1)
+
+torch_time = torch_time / 10 / 1_000_000   
+
+print(f"Triton time: {triton_time}ms")
+print(f"Torch time: {torch_time}ms")
 
 try:
     torch.testing.assert_close(triton_output, torch_output, atol=1e-2, rtol=0)
