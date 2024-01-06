e70e1962cc6135dec19ab7b24ffc5a8cd6c5a1d5
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2024-01-06 07:13:56 -0800

    Add more MLP models

--- linear_layer_triton/src/mlp_models.py
+++ linear_layer_triton/src/mlp_models.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from transformers.activations import NewGELUActivation
 
 class MLP1(nn.Module):
     def __init__(self):
@@ -30,3 +31,35 @@ class MLP3(nn.Module):
         x = self.gelu(x)
         x = self.ll2(x)
         return x
+
+class MLP4(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.ll1 = nn.Linear(1024, 512)
+        self.ll2 = nn.Linear(512, 1024)
+        self.dropout = nn.Dropout()
+        self.gelu = NewGELUActivation()
+
+    def forward(self, x):
+        x = self.gelu(self.ll1(x))
+        x = self.dropout(x)
+        x = self.ll2(x)
+        return x
+    
+class MLP5(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.ll1 = nn.Linear(1024, 512)
+        self.ll2 = nn.Linear(1024, 512)
+        self.ll3 = nn.Linear(512, 1024)
+        self.dropout = nn.Dropout()
+        self.gelu = NewGELUActivation()
+
+    def forward(self, x):
+        hidden_gelu = self.gelu(self.ll1(x))
+        hidden_linear = self.ll2(x)
+        x = hidden_linear * hidden_gelu
+        x = self.dropout(x)
+        x = self.ll3(x)
+        return x
+    
