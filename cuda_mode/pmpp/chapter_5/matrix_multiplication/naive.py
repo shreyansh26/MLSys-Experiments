76bc53679bf5e1016b6d358a0fca80a14df654d3
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2024-02-09 11:11:09 -0800

    Add tiled smem matmul

---
+++ cuda_mode/pmpp/chapter_5/matrix_multiplication/naive.py
@@ -0,0 +1,62 @@
+from torch.utils.cpp_extension import load_inline
+import os
+import torch
+
+os.environ['CUDA_LAUNCH_BLOCKING']='1'
+
+cuda_begin = r'''
+#include <torch/extension.h>
+#include <stdio.h>
+#include <c10/cuda/CUDAException.h>
+
+#define CHECK_CUDA(x) TORCH_CHECK(x.device().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
+#define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
+
+inline unsigned int cdiv(unsigned int a, unsigned int b) { return (a + b - 1) / b;}
+'''
+
+cuda_src = cuda_begin + r'''
+__global__ void matmul_k(float* m, float* n, float* out, int h, int w, int k) {
+    int r = blockIdx.y*blockDim.y + threadIdx.y;
+    int c = blockIdx.x*blockDim.x + threadIdx.x;
+
+    if (r>=h || c>=w) return;
+    float o = 0;
+    for (int i = 0; i<k; ++i) o += m[r*k+i] * n[i*w+c];
+    out[r*w+c] = o;
+}
+
+torch::Tensor matmul(torch::Tensor m, torch::Tensor n) {
+    CHECK_INPUT(m); CHECK_INPUT(n);
+    int h = m.size(0);
+    int w = n.size(1);
+    int k = m.size(1);
+    TORCH_CHECK(k==n.size(0), "Size mismatch!");
+    auto output = torch::zeros({h, w}, m.options());
+
+    dim3 tpb(16,16);
+    dim3 blocks(cdiv(w, tpb.x), cdiv(h, tpb.y));
+    matmul_k<<<blocks, tpb>>>(
+        m.data_ptr<float>(), n.data_ptr<float>(), output.data_ptr<float>(), h, w, k);
+    C10_CUDA_KERNEL_LAUNCH_CHECK();
+    return output;
+}
+'''
+
+def load_cuda(cuda_src, cpp_src, funcs, opt=False, verbose=False):
+    return load_inline(cuda_sources=[cuda_src], cpp_sources=[cpp_src], functions=funcs,
+                       extra_cuda_cflags=["-O2"] if opt else [], verbose=verbose, name="inline_ext")
+
+cpp_src = "torch::Tensor matmul(torch::Tensor m, torch::Tensor n);"
+
+module = load_cuda(cuda_src, cpp_src, ['matmul'])
+
+m1 = torch.randn(512, 512)
+m2 = torch.randn(512, 512)
+
+m1c, m2c = m1.contiguous().cuda(), m2.contiguous().cuda()
+
+tr = torch.matmul(m1, m2)
+
+print(torch.isclose(tr, module.matmul(m1c, m2c).cpu(), atol=1e-4).all())
---
+++ cuda_mode/pmpp/chapter_5/matrix_multiplication/tiled_smem.cu
@@ -0,0 +1,101 @@
+#include <cuda.h>
+#include <stdio.h>
+
+#define TILE_WIDTH 32
+
+// A is M X K matrix and B is K x N matrix
+// O is M x N matrix
+__global__
+void matmul_kernel(float* O, float* A, float* B, int M, int K, int N) {
+    int c = blockIdx.x * TILE_WIDTH + threadIdx.x;
+    int r = blockIdx.y * TILE_WIDTH + threadIdx.y;
+
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    __shared__ float As[TILE_WIDTH][TILE_WIDTH];
+    __shared__ float Bs[TILE_WIDTH][TILE_WIDTH];
+
+    float sum = 0.0f;
+    for(int i=0; i<ceil(K/(float)TILE_WIDTH); i++) {
+        if(r < M and (i*TILE_WIDTH + tx) < K)
+            As[ty][tx] = A[r*K + i*TILE_WIDTH + tx];
+        else
+            As[ty][tx] = 0.0f;
+
+        if((i*TILE_WIDTH + ty) < K and c < N)
+            Bs[ty][tx] = B[(i*TILE_WIDTH + ty)*N + c];
+        else
+            Bs[ty][tx] = 0.0f;
+            
+        __syncthreads();
+
+        for(int ii=0; ii<TILE_WIDTH; ii++) {
+            sum += As[ty][ii] * Bs[ii][tx];
+        }
+        __syncthreads();
+    }
+    if(r < M and c < N)
+        O[r*N + c] = sum;
+}
+
+inline unsigned int cdiv(unsigned int a, unsigned int b) {
+    return (a + b - 1)/b;
+}
+
+void matmul(float* O_h, float* A_h, float* B_h, int M, int K, int N) {
+    float *O_d;
+    float *A_d; 
+    float *B_d;
+
+    cudaMalloc((void **)&O_d, M*N*sizeof(float));
+    cudaMalloc((void **)&A_d, M*K*sizeof(float));
+    cudaMalloc((void **)&B_d, K*N*sizeof(float));
+
+    cudaMemcpy(A_d, A_h, M*K*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d, B_h, K*N*sizeof(float), cudaMemcpyHostToDevice);
+
+    dim3 blockSize(TILE_WIDTH, TILE_WIDTH);
+    dim3 gridSize(cdiv(N, blockSize.x), cdiv(M, blockSize.y));
+
+    matmul_kernel<<<gridSize, blockSize>>>(O_d, A_d, B_d, M, K, N);
+
+    cudaMemcpy(O_h, O_d, M*N*sizeof(float), cudaMemcpyDeviceToHost);
+
+    cudaFree(O_d);
+    cudaFree(A_d);
+    cudaFree(B_d);
+}
+
+int main() {
+    int M = 64;
+    int K = 64;
+    int N = 128;
+
+    float  *mat1 = new float[M*K];
+    float  *mat2 = new float[K*N];
+    float  *out = new float[M*N];
+
+    for (int h = 0; h < M; h++){
+        for (int w = 0; w < K; w++)
+            mat1[K * h + w] = w;
+    }
+
+    for (int h = 0; h < K; h++){
+        for (int w = 0; w < N; w++)
+            mat2[N * h + w] = w;
+    }
+
+    matmul(out, mat1, mat2, M, K, N);
+
+    for(int i=0; i<M; i++) {
+        for(int j=0; j<N; j++) {
+            if(j > 0)
+                printf(", ");
+            printf("%8.3f", out[i*N+j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    return 0;
+}
