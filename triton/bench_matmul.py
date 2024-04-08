89e72b900cfa20b897f645de7d837f6f3da2d787
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2024-04-08 08:25:16 -0700

    Obscure matmul bench

--- triton/bench_matmul.py
+++ triton/bench_matmul.py
@@ -1,5 +1,6 @@
 import torch
 from triton.testing import do_bench
+from triton.ops import matmul
 
 torch.set_default_device('cuda')
 
@@ -20,3 +21,23 @@ for M, K, N in [(2047, 2048, 2048), (2048, 2047, 2048), (2048, 2048, 2047)]:
     print(f"M={M}, K={K}, N={N}")
 
     print(do_bench(lambda : torch.mm(A, B)))
+
+
+M = 4096
+K = 4096
+N = 4096
+
+A = torch.randn(M, K, dtype=torch.bfloat16)
+B = torch.randn(K, N, dtype=torch.bfloat16)
+
+print(f"M={M}, K={K}, N={N}")
+print("Torch mm", do_bench(lambda : torch.mm(A, B)))
+print("Triton mm", do_bench(lambda : matmul(A, B)))
+
+A = torch.randn(M, K, dtype=torch.bfloat16)
+B = torch.randn(K, N, dtype=torch.bfloat16)
+B = B[:, :4095]
+
+print(f"M={M}, K={K}, N={N}")
+print("Torch mm", do_bench(lambda : torch.mm(A, B)))
+print("Triton mm", do_bench(lambda : matmul(A, B)))
