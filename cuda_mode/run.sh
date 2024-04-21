cb029edcd91e020430591ed0994d6bcbb24dac7b
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2024-04-20 22:18:12 -0700

    Add Triton lecture notebook

--- cuda_mode/run.sh
+++ cuda_mode/run.sh
@@ -1,1 +1,1 @@
-docker run --gpus '"device=0"' -v /mnt/ssd1/shreyansh/home_dir/misc_experiments/cuda_mode/pmpp/:/workspace/pmpp --cap-add=SYS_ADMIN -it --rm pytorch-profile
+docker run --gpus '"device=0"' -v /mnt/ssd1/shreyansh/home_dir/misc_experiments/cuda_mode/pmpp/:/workspace/pmpp -it --rm nvcr.io/nvidia/pytorch:24.01-py3
