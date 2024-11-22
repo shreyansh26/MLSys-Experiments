27ce26a28094214a5e221ddcf0df613137a69276
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2024-11-22 10:28:27 -0800

    Add more comments for understandability

--- cuda_mode/matmul/src/kernels/03_shared_memory.cuh
+++ cuda_mode/matmul/src/kernels/03_shared_memory.cuh
@@ -10,10 +10,12 @@ __global__ void sgemm_shared_memory(int M, int N, int K, float alpha, const floa
     int cRow = blockIdx.x;
     int cCol = blockIdx.y;
 
-    A += cRow * BLOCKDIM * K;
-    B += cCol * BLOCKDIM;
-    C += cRow * BLOCKDIM * N + cCol * BLOCKDIM;
+    // advance pointers to the starting positions
+    A += cRow * BLOCKDIM * K;                       // row=cRow, col=0
+    B += cCol * BLOCKDIM;                           // row=0, col=cCol
+    C += cRow * BLOCKDIM * N + cCol * BLOCKDIM;     // row=cRow, col=cCol
 
+    // the inner row & col that we're accessing in this thread
     int threadRow = threadIdx.x / BLOCKDIM;
     int threadCol = threadIdx.x % BLOCKDIM;
 
@@ -26,17 +28,22 @@ __global__ void sgemm_shared_memory(int M, int N, int K, float alpha, const floa
         As[threadRow * BLOCKDIM + threadCol] = A[threadRow * K + threadCol];
         Bs[threadRow * BLOCKDIM + threadCol] = B[threadRow * N + threadCol];
 
+        // block threads in this block until cache is fully populated
         __syncthreads();
 
+        // execute the dotproduct on the currently cached block
         for(int bdim = 0; bdim < BLOCKDIM; bdim++) {
             sum += As[threadRow * BLOCKDIM + bdim] * Bs[bdim * BLOCKDIM + threadCol];
         }
 
+        // need to sync again at the end, to avoid faster threads
+        // fetching the next block into the cache before slower threads are done
         __syncthreads();
 
         A += BLOCKDIM;
         B += BLOCKDIM * N;
     }
 
+    // update the C matrix
     C[threadRow * N + threadCol] = alpha * sum + beta * C[threadRow * N + threadCol];
 }
