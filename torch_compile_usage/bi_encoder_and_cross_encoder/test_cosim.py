b0d192cbc910714cf14bc050e4233b6f7e2f3848
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-01-31 21:57:11 -0800

    Add ST vs non-ST test

--- torch_compile_usage/bi_encoder_and_cross_encoder/test_cosim.py
+++ torch_compile_usage/bi_encoder_and_cross_encoder/test_cosim.py
@@ -1,18 +1,25 @@
 import numpy as np
 
+st_simple_embeddings = np.load("outputs/basic_simple_embeddings.npy")
 simple_embeddings = np.load("outputs/simple_embeddings.npy")
 opt_embeddings = np.load("outputs/opt_embeddings.npy")
 
 print(simple_embeddings.shape)
 print(opt_embeddings.shape)
 
-# Get cosine similarity between all pairs of embeddings corresponding to the same sentence
-cosine_sim = np.dot(simple_embeddings, opt_embeddings.T) / (np.linalg.norm(simple_embeddings, axis=1) * np.linalg.norm(opt_embeddings, axis=1))
+cosine_sim = np.dot(simple_embeddings, st_simple_embeddings.T) / (np.linalg.norm(simple_embeddings, axis=1) * np.linalg.norm(st_simple_embeddings, axis=1))
 
+print("Cosine similarity between ST and non-ST embeddings:")
 print(cosine_sim.shape)
 
 print(cosine_sim.diagonal())
-# [0.99999964 0.9999995  0.9999993  ... 0.9999996  0.99999887 0.99999887]
-
 print(cosine_sim.diagonal().sum(), cosine_sim.shape[0])
-# 4095.9976 4096
+
+
+cosine_sim = np.dot(simple_embeddings, opt_embeddings.T) / (np.linalg.norm(simple_embeddings, axis=1) * np.linalg.norm(opt_embeddings, axis=1))
+
+print("Cosine similarity between non-ST and OPT embeddings:")
+print(cosine_sim.shape)
+
+print(cosine_sim.diagonal())
+print(cosine_sim.diagonal().sum(), cosine_sim.shape[0])
