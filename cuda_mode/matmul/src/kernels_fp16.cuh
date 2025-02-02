08f698b7a0b02d58b9a022288ec4153261bcf9e4
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-02-02 03:49:47 -0800

    Update kernel header files

--- cuda_mode/matmul/src/kernels_fp16.cuh
+++ cuda_mode/matmul/src/kernels_fp16.cuh
@@ -1,3 +1,5 @@
 #pragma once
 
 #include "kernels_fp16/01_hierarchical_tiling.cuh"
+#include "kernels_fp16/02_hierarchical_tiling_unrolled.cuh"
+#include "kernels_fp16/03_hierarcical_tiling_unrolled_vectorized.cuh"
