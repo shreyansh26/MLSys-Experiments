184d91845398d173a3e91f93791bf00901e4093f
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-02-03 19:48:22 -0800

    Update min size of matrix for benchmarking

--- cuda_mode/matmul/sgemm_fp16.cu
+++ cuda_mode/matmul/sgemm_fp16.cu
@@ -69,7 +69,7 @@ int main(int argc, char **argv) {
     cudaEventCreate(&end);
 
     // cuBLAS FLOPs ceiling is reached at 8192
-    std::vector<int> SIZE = {128, 256, 512, 1024, 2048, 4096};
+    std::vector<int> SIZE = {256, 512, 1024, 2048, 4096};
 
     long m, n, k, max_size;
     max_size = SIZE[SIZE.size() - 1];
