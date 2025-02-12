e445005134c688fa000995f8b08f5a66fc51769c
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-02-12 08:12:59 -0800

    Tuen tile dimensions to decrease shared memory storage and increasing arithemtic intensity

--- cuda_mode/matmul/src/kernels_fp16.cuh
+++ cuda_mode/matmul/src/kernels_fp16.cuh
@@ -5,3 +5,4 @@
 #include "kernels_fp16/03_hierarcical_tiling_unrolled_vectorized.cuh"
 #include "kernels_fp16/04_shared_memory_swizzling.cuh"
 #include "kernels_fp16/05_async_copy.cuh"
+#include "kernels_fp16/06_tuning_tile_dimensions.cuh"
