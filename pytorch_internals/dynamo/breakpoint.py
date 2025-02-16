a568cb6281604ab7d02f6e133cf39422d7147b1e
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-02-16 07:23:32 -0800

    Add functions useful for debugging

--- pytorch_internals/dynamo/breakpoint.py
+++ pytorch_internals/dynamo/breakpoint.py
@@ -11,3 +11,15 @@ f_compiled = torch.compile(f)
 x = torch.tensor(1)
 print(f_compiled(x))
 print(f(x))
+
+'''
+ctx.print_bt() to print the user stack trace
+
+ctx.print_locals() to print all current locals
+
+ctx.print_graph() to print the currently traced graph
+
+ctx.disas() to print the currently traced function’s bytecode
+
+Use standard pdb commands, such as bt/u/d/n/s/r, - you can go up the pdb stack to inspect more Dynamo internals
+'''
