dc88302f25ababa2caac5a36516afe69bb2186db
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-02-15 19:31:54 -0800

    TORCH_LOGS=aot_graphs prints forward and backward graph

---
+++ pytorch_internals/aotautograd/log_aot_graphs.py
@@ -0,0 +1,14 @@
+# TORCH_LOGS=aot_graphs python log.py
+import torch
+
+def fn(a, b, c, d):
+    x = a + b + c + d
+    return x.cos().cos()
+
+# Test that it works
+a, b, c, d = [torch.randn(2, 4, requires_grad=True) for _ in range(4)]
+
+fn = torch.compile(fn)
+ref = fn(a, b, c, d)
+loss = ref.sum()
+loss.backward()
