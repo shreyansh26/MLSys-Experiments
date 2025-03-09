50ff6995caa3f7b611fa343182fcd139c3d73f6b
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-03-09 00:00:45 -0800

    Add cublas implementation

---
+++ cuda_mode/matmul/src/run_bf16.cu
@@ -0,0 +1,41 @@
+#include "run_bf16.cuh"
+#include "kernels_bf16.cuh"
+#include "cuda_utils.cuh"
+
+typedef __nv_bfloat16 bf16;
+#define cudaCheck(val) check_cuda((val), #val, __FILE__, __LINE__)
+
+void run_cublas(cublasHandle_t handle, int M, int N, int K, float alpha, bf16 *A, bf16 *B, float beta, bf16 *C) {
+    // A is MxK, B is NxK, C is MxN (row major)
+    // So if B (first argument) is column major - KxN and then transposed - NxK
+    // And A is simply column major - KxM
+    // So C is NxM (column major) -> MxN (row major)
+    cublasStatus_t status = cublasGemmEx(handle,
+        CUBLAS_OP_T,
+        CUBLAS_OP_N,
+        N, M, K,
+        &alpha,
+        B, CUDA_R_16BF, K,
+        A, CUDA_R_16BF, K,
+        &beta,
+        C, CUDA_R_16BF, N,
+        CUBLAS_COMPUTE_32F,
+        CUBLAS_GEMM_DEFAULT
+    );
+
+    if (status != CUBLAS_STATUS_SUCCESS) {
+        std::cerr << "cublasGemmEx failed with status " << status << std::endl;
+        exit(1);
+    }
+}
+
+void run_kernel_bf16(int kernel_num, int M, int N, int K, float alpha, bf16 *A, bf16 *B, float beta, bf16 *C, cublasHandle_t handle) {
+    switch (kernel_num) {
+        case 0:
+            // std::cout << "cuBLAS BF16" << std::endl;
+            run_cublas(handle, M, N, K, alpha, A, B, beta, C);
+            break;
+        default:
+            throw std::invalid_argument("Invalid kernel number");
+    }
+}
---
+++ cuda_mode/matmul/src/run_bf16.cuh
@@ -0,0 +1,19 @@
+#pragma once
+#include <cublas_v2.h>
+#include <cuda_bf16.h>
+#include <cuda_runtime.h>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <sys/time.h>
+#include <unistd.h>
+#include <iostream>
+#include <iomanip>
+
+typedef __nv_bfloat16 bf16;
+
+void check_cuda(cudaError_t err, const char* const func, const char* const file, const int line);
+
+void cuda_device_info();
+
+void run_kernel_bf16(int kernel_num, int m, int n, int k, float alpha, bf16 *A, bf16 *B, float beta, bf16 *C, cublasHandle_t handle);
