8b00c591845327270496363229a91a3bba348bb2
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-04-10 04:37:09 -0700

    Add kernel 10 with async stores

---
+++ cuda_mode/matmul/src/kernels_bf16_h100/10_async_stores.cuh
@@ -0,0 +1,140 @@
+#pragma once
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cuda_bf16.h>
+#include <cudaTypedefs.h>
+#include <cuda/barrier>
+#include "hopper_utils.cuh"
+#include "data_utils.cuh"
+
+typedef __nv_bfloat16 bf16;
+using barrier = cuda::barrier<cuda::thread_scope_block>;
+namespace cde = cuda::device::experimental;
+
+template<int BM, int BN, int BK, int NUM_THREADS, int QSIZE>
+__global__ void __launch_bounds__(NUM_THREADS) async_stores(int M, int N, int K, bf16* C, const CUtensorMap* tensorMapC, const CUtensorMap* tensorMapA, const CUtensorMap* tensorMapB, float alpha, float beta, int *DB) {
+    constexpr int WGMMA_M = 64, WGMMA_K = 16, WGMMA_N = BN;
+    constexpr int num_consumers = (NUM_THREADS / 128) - 1; // number of consumer warp groups
+    constexpr int B_WG_M = BM / num_consumers;
+
+    // extern __shared__  SMemQueue<BM, BN, BK, QSIZE> smem_queue;
+    // bf16 *sA = smem_queue.A;
+    // bf16 *sB = smem_queue.B;
+
+    extern __shared__ __align__(128) uint8_t smem_queue[];
+    SMemQueue2<BM, BN, BK, QSIZE> &s = *reinterpret_cast<SMemQueue2<BM, BN, BK, QSIZE>*>(smem_queue);
+    bf16 *sA = s.A;
+    bf16 *sB = s.B;
+    bf16 *sC = s.C;
+
+    const int num_blocks_k = K / BK;
+    int num_block_n = blockIdx.x % (N / BN);
+    int num_block_m = blockIdx.x / (N / BN);
+    int wg_idx = threadIdx.x / 128;
+    int tid = threadIdx.x % 128;
+
+    #pragma nv_diag_suppress static_var_with_dynamic_init
+    __shared__ barrier full[QSIZE];
+    __shared__ barrier empty[QSIZE];
+
+    if (threadIdx.x == 0) {
+        for (int i = 0; i < QSIZE; ++i) {
+            init(&full[i], num_consumers * 128 + 1); // all consumer + 1 producer
+            init(&empty[i], num_consumers * 128 + 1); // all consumer + 1 producer
+        }
+        cde::fence_proxy_async_shared_cta(); // barrier and memory initialization visible to all warps
+    }
+    __syncthreads();
+
+    if(wg_idx == 0) { // producer
+        /*
+        “full” barrier signals that data has been loaded by the producer
+        “empty” barrier signals that the consumer has used the data and the slot is available
+        */
+        constexpr int num_regs = (num_consumers <= 2 ? 24 : 32);
+        warpgroup_reg_dealloc<num_regs>();
+        
+        if(tid == 0) {
+            int qidx = 0;
+            for (int block_k_iter = 0; block_k_iter < num_blocks_k; ++block_k_iter, ++qidx) {
+                if(qidx == QSIZE) // ring buffer
+                    qidx = 0;
+                empty[qidx].wait(empty[qidx].arrive());
+                cde::cp_async_bulk_tensor_2d_global_to_shared(&sA[qidx*BM*BK], tensorMapA, block_k_iter*BK, num_block_m*BM, full[qidx]); // col offset, row offset -> sA is row major
+                cde::cp_async_bulk_tensor_2d_global_to_shared(&sB[qidx*BK*BN], tensorMapB, block_k_iter*BK, num_block_n*BN, full[qidx]); // col offset, row offset -> sB is row major
+                barrier::arrival_token _ = cuda::device::barrier_arrive_tx(full[qidx], 1, (BK*BN+BK*BM)*sizeof(bf16));
+            }
+        }
+    }
+    else {
+        constexpr int num_regs = (num_consumers == 1 ? 256 : (num_consumers == 2 ? 240 : 160));
+        warpgroup_reg_alloc<num_regs>();
+        --wg_idx;
+
+        for(int i = 0; i < QSIZE; ++i) {
+            barrier::arrival_token _ = empty[i].arrive();
+        }
+
+        float d[B_WG_M/WGMMA_M][WGMMA_N/16][8];
+        memset(d, 0, sizeof(d));
+        int qidx = 0;
+        for (int block_k_iter = 0; block_k_iter < num_blocks_k; ++block_k_iter, ++qidx) {
+            if(qidx == QSIZE) 
+                qidx = 0;
+            full[qidx].wait(full[qidx].arrive());
+            warpgroup_arrive();
+            #pragma unroll
+            for(int m_it = 0; m_it < B_WG_M/WGMMA_M; ++m_it) {
+                bf16 *wgmma_sA = sA + qidx*BK*BM + (m_it + wg_idx * B_WG_M / WGMMA_M) * WGMMA_M * BK;
+                #pragma unroll 
+                for (int k_it = 0; k_it < BK/WGMMA_K; ++k_it) {
+                    wgmma<WGMMA_N, 1, 1, 1, 0, 0>(d[m_it], &wgmma_sA[k_it * WGMMA_K], &sB[qidx*BK*BN + k_it*WGMMA_K]);
+                }
+            }
+            warpgroup_commit_batch();
+            warpgroup_wait<0>();
+            barrier::arrival_token _ = empty[qidx].arrive();
+        }
+
+        // Store
+        {
+            int tid = threadIdx.x % 128;
+            int lane = tid % 32;
+            int warp = tid / 32;
+            uint32_t row = warp*16 + lane / 4;
+            
+            asm volatile("cp.async.bulk.wait_group 0;");
+            bf16 *block_sC = sC + wg_idx * B_WG_M * BN;
+
+            #pragma unroll
+            for (int m_it = 0; m_it < B_WG_M/WGMMA_M; ++m_it) {
+                int offset_m = m_it * WGMMA_M;
+                #pragma unroll
+                for (int w = 0; w < WGMMA_N/16; ++w) {
+                    int col = 16*w + 2*(tid % 4);
+                    #define IDX(i, j) ((j)*B_WG_M + ((i) + offset_m))
+                    #define ST(i, j, v) block_sC[IDX(i, j)] = v;
+
+                    ST(row, col, d[m_it][w][0]);
+                    ST(row+8, col, d[m_it][w][2]);
+                    ST(row, col+1, d[m_it][w][1]);
+                    ST(row+8, col+1, d[m_it][w][3]);
+
+                    ST(row, col+8, d[m_it][w][4]);
+                    ST(row+8, col+8, d[m_it][w][6]);
+                    ST(row, col+9, d[m_it][w][5]);
+                    ST(row+8, col+9, d[m_it][w][7]);
+
+                    #undef ST
+                    #undef IDX
+                }
+            }
+
+            asm volatile("bar.sync 10, 256;\n");
+            if (threadIdx.x == 128) {
+                store_async(&tensorMapC, (bf16*)&sC[0], num_block_m*BM, num_block_n*BN);
+                asm volatile("cp.async.bulk.commit_group;");
+            }
+        }
+    }
+}
