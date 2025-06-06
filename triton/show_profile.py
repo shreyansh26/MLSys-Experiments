4a6ef9469ef20fc9a9ed4b4c554ee697ca3c2255
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-06-06 02:34:48 -0700

    Add profile show code

---
+++ triton/show_profile.py
@@ -0,0 +1,14 @@
+import triton.profiler.viewer as proton_viewer
+
+def show_profile(precision, profile_name):
+    metric_names = ["time/ms"]
+    if precision == 'fp8':
+        metric_names = ["tflop8/s"] + metric_names
+    elif precision == 'fp16':
+        metric_names = ["tflop16/s"] + metric_names
+    file_name = f"{profile_name}.hatchet"
+    tree, metrics = proton_viewer.parse(metric_names, file_name)
+    proton_viewer.print_tree(tree, metrics)
+
+if __name__ == "__main__":
+    show_profile("fp16", "proton_results/matmul_fp16")
