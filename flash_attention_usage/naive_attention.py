d39d0dcd4a2df5440300777120edaa4072736790
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-07-21 20:45:38 -0700

    Modify naive attn impl to support causal-ish attention masks as well

--- flash_attention_usage/naive_attention.py
+++ flash_attention_usage/naive_attention.py
@@ -1,12 +1,13 @@
 import torch
 import math
 
-# Naive causal attention
 def naive_causal_attention(q, k, v):
-    B, seq_len, nheads, headdim = q.shape
+    B, q_seq_len, nheads, headdim = q.shape
+    _, k_seq_len, _, _ = k.shape
+    diag = k_seq_len - q_seq_len
     attention_weights = torch.einsum("nqhd,nkhd->nhqk", [q, k])    
     attention_weights = attention_weights / math.sqrt(headdim)
-    attention_mask = torch.tril(torch.ones(seq_len, seq_len, device=q.device))
+    attention_mask = torch.tril(torch.ones(q_seq_len, k_seq_len, device=q.device), diagonal=diag)
     attention_weights = attention_weights.masked_fill(attention_mask == 0, float('-inf'))
     attention_weights = torch.softmax(attention_weights, dim=-1)
     out = torch.einsum("nhqk,nkhd->nqhd", [attention_weights, v])
