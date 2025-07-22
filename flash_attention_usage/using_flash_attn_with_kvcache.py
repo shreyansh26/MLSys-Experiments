187e86e8f38b5c3edec4b8a3539ac54e72bd2fd0
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-07-21 20:45:10 -0700

    Add verification for kvcache API

--- flash_attention_usage/using_flash_attn_with_kvcache.py
+++ flash_attention_usage/using_flash_attn_with_kvcache.py
@@ -1,5 +1,8 @@
 import torch
 from flash_attn import flash_attn_with_kvcache
+from naive_attention import naive_causal_attention
+
+VERIFY = True
 
 B, max_seq_len, nheads, headdim = 2, 4096, 16, 64      # max sequence length for cache
 dtype = torch.float16
@@ -23,4 +26,20 @@ for step in range(50):                # generate 50 tokens
     )
 
     cache_lens += 1                      # advance sequence lengths
+
+    if VERIFY:
+        batch_idx = torch.arange(B, device=k_cache.device) 
+        k_newly_added = k_cache[batch_idx, cache_lens-1].unsqueeze(1)
+        v_newly_added = v_cache[batch_idx, cache_lens-1].unsqueeze(1)
+
+        torch.testing.assert_close(k_newly_added, k_new, atol=1e-3, rtol=1e-2)
+        torch.testing.assert_close(v_newly_added, v_new, atol=1e-3, rtol=1e-2)
+
+        seq_len = cache_lens[0].item()
+        k_cache_naive = k_cache[batch_idx, :seq_len]
+        v_cache_naive = v_cache[batch_idx, :seq_len]
+
+        out_naive = naive_causal_attention(q, k_cache_naive, v_cache_naive)
+        torch.testing.assert_close(out, out_naive, atol=1e-3, rtol=1e-2)
+                          
     print(f"step {step} out.shape: {out.shape}")
