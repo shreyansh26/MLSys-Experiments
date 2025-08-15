0a008fd1966b7be0bc91b3c6be6c0d78041f2241
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-08-15 04:12:02 -0700

    Use KV Cache using FA API

---
+++ flash_attention_usage/inference/generation_kv_cache.py
@@ -0,0 +1,166 @@
+import json
+import time
+from typing import Optional
+import torch
+from model_llama_kv_cache import Transformer
+from tokenizer_llama import Tokenizer
+from dataclasses import dataclass
+
+def multinomial_sample_one(
+    probs: torch.Tensor, rng: Optional[torch.Generator] = None
+) -> torch.Tensor:
+    q = torch.empty_like(probs).exponential_(1, generator=rng)
+    return torch.argmax(probs / q, dim=-1, keepdim=True).to(dtype=torch.long)
+
+
+def logits_to_probs(
+    logits: torch.Tensor,
+    temperature: float = 1.0,
+    top_k: Optional[int] = None,
+) -> torch.Tensor:
+    logits = logits / max(temperature, 1e-5)
+
+    if top_k is not None:
+        v, _ = torch.topk(logits, k=min(top_k, logits.size(-1)))
+        pivot = v.select(dim=-1, index=-1).unsqueeze(-1)
+        logits = torch.where(logits < pivot, -float("Inf"), logits)
+
+    probs = torch.nn.functional.softmax(logits, dim=-1)
+    return probs
+
+
+def generate_next_token(
+    model,
+    x: torch.Tensor,
+    kv_cache: list,
+    *,
+    temperature: float = 0.0,
+    top_k: Optional[int] = None,
+    rng: Optional[torch.Generator] = None,
+    curr_idx: int,
+) -> torch.Tensor:
+    logits = model(x, kv_cache=kv_cache, curr_idx=curr_idx)  # (B, T, vocab_size)
+    probs = logits_to_probs(logits[:, -1, :], temperature, top_k)
+    next_token = multinomial_sample_one(probs, rng=rng)
+    return next_token
+
+
+@torch.no_grad()
+def generate(
+    model,
+    input_ids: torch.Tensor,
+    *,
+    max_new_tokens: int,
+    temperature: float = 0.0,
+    top_k: Optional[int] = None,
+    seed: Optional[int] = None,
+) -> torch.Tensor:
+    # ensure batch dimension (T,) --> (B, T)
+    if input_ids.ndim == 1:
+        input_ids = input_ids.unsqueeze(0)
+
+    rng = None
+    if seed is not None:
+        rng = torch.Generator(input_ids.device).manual_seed(seed)
+
+    head_dim = model.model_args.dim // model.model_args.n_heads
+
+    # Allocate enough space for prompt + generated tokens
+    prompt_len = input_ids.shape[1]
+    cache_len = prompt_len + max_new_tokens
+    kv_cache = [(
+        torch.zeros((input_ids.shape[0], cache_len, model.model_args.n_kv_heads, head_dim), dtype=torch.bfloat16, device="cuda"),
+        torch.zeros((input_ids.shape[0], cache_len, model.model_args.n_kv_heads, head_dim), dtype=torch.bfloat16, device="cuda"),
+    ) for _ in range(model.model_args.n_layers)]
+
+    generated_tokens = input_ids.clone()
+    next_token = input_ids.clone()
+
+    prompt_len = input_ids.shape[1]
+    curr_idx = 0
+
+    for idx in range(prompt_len, prompt_len + max_new_tokens):
+        next_token = generate_next_token(
+            model,
+            x=next_token,
+            kv_cache=kv_cache,
+            temperature=temperature,
+            top_k=top_k,
+            rng=rng,
+            curr_idx=curr_idx,
+        )
+
+        generated_tokens = torch.cat([generated_tokens, next_token], dim=1)
+        curr_idx = idx
+
+    return generated_tokens
+
+def load_model(model_path, model_args):
+    with torch.device("meta"):
+        model = Transformer(model_args)
+    
+    model = model.to_empty(device="cpu")
+    state_dict = torch.load(f"{model_path}/consolidated.00.pth", weights_only=True, mmap=True)
+    model.load_state_dict(state_dict, assign=True)
+
+    # Load freqs_cis separately
+    with torch.no_grad():
+        model.freqs_cis = model._precompute_freqs_cis()
+    return model
+
+def load_model2(model_path, model_args):
+    model = Transformer(model_args)
+    
+    state_dict = torch.load(f"{model_path}/consolidated.00.pth", weights_only=True, mmap=True)
+    model.load_state_dict(state_dict, assign=True)
+
+    return model
+
+@dataclass
+class ModelArgs:
+    dim: int = None
+    n_layers: int = None
+    n_heads: int = None
+    n_kv_heads: int = None
+    vocab_size: int = None
+    ffn_dim_multiplier: float = None
+    multiple_of: int = None
+    norm_eps: float = None
+    rope_theta: float = None
+    use_scaled_rope: bool = None
+    max_seq_len: int = None
+
+if __name__ == "__main__":
+    model_name = "llama_3b_instruct"
+    model_path = f"./{model_name}/original"
+    model_config = f"{model_path}/params.json"
+    with open(model_config, "r") as f:
+        params = json.load(f)
+
+    params['max_seq_len'] = 131072
+    model_args = ModelArgs(**params)
+    print(model_args)
+
+    time_start = time.time()
+    model = load_model(model_path, model_args)
+    time_end = time.time()
+    print(f"Model loading time: {time_end - time_start} seconds")
+
+    time_start = time.time()
+    model.to("cuda")
+    time_end = time.time()
+    print(f"Model to GPU transfer time: {time_end - time_start} seconds")
+
+    tokenizer = Tokenizer(f"{model_path}/tokenizer.model")
+    time_end = time.time()
+    print(f"Tokenizer loading time: {time_end - time_start} seconds")
+
+    prompt = "Hello, who are you?"
+    tokens = tokenizer.encode(prompt, bos=True, eos=False)
+    input_ids = torch.tensor(tokens).unsqueeze(0).to("cuda")
+
+    time_start = time.time()
+    output = generate(model, input_ids, max_new_tokens=100)
+    time_end = time.time()
+    print(f"Generation time: {time_end - time_start} seconds")
+    print(tokenizer.decode(output[0].tolist()))
---
+++ flash_attention_usage/inference/model_llama_kv_cache.py
@@ -0,0 +1,396 @@
+import torch
+import torch.nn.functional as F
+from torch import nn
+from flash_attn import flash_attn_with_kvcache
+
+# From torchtitan codebase - https://github.com/pytorch/torchtitan/blob/6fc499f6f5b32151a799188be2208cfb09faed30/torchtitan/models/llama3/model/model.py
+# class KVCache:
+#     def __init__(self, model_args, batch_size, max_len=None, device="cuda"):
+#         self.model_args = model_args
+#         self.kv_cache = []
+#         self.kv_cache_size = max_len if max_len is not None else model_args.max_seq_len
+#         self.head_dim = model_args.dim // model_args.n_heads
+#         self.batch_size = batch_size
+
+#         self.kv_cache = {str(i): (torch.zeros(self.batch_size, self.kv_cache_size, self.model_args.n_kv_heads, self.head_dim, device=device), 
+#                             torch.zeros(self.batch_size, self.kv_cache_size, self.model_args.n_kv_heads, self.head_dim, device=device)) for i in range(model_args.n_layers)}
+
+#     def get_kv_cache(self, layer_id, pos_to_get):
+#         return self.kv_cache[layer_id][0][:, pos_to_get], self.kv_cache[layer_id][1][:, pos_to_get]
+        
+#     def update_kv_cache(self, layer_id, kv_cache_new):
+#         k, v = kv_cache_new
+#         self.kv_cache[layer_id]
+
+def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0) -> torch.Tensor:
+    """
+    Precompute the frequency tensor for complex exponentials (cis) with given dimensions.
+
+    This function calculates a frequency tensor with complex exponentials using the given dimension 'dim'
+    and the end index 'end'. The 'theta' parameter scales the frequencies.
+    The returned tensor contains complex values in complex64 data type.
+
+    Args:
+        dim (int): Dimension of the frequency tensor.
+        end (int): End index for precomputing frequencies.
+        theta (float | None): Scaling factor for frequency computation. Defaults to 10000.0.
+
+    Returns:
+        torch.Tensor: Precomputed frequency tensor with complex exponentials.
+    """
+    freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
+    t = torch.arange(end, device=freqs.device)
+    freqs = torch.outer(t, freqs).float()
+    freqs_cis = torch.polar(torch.ones_like(freqs), freqs)  # complex64
+    return freqs_cis
+
+
+def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor) -> torch.Tensor:
+    """
+    Reshape frequency tensor for broadcasting it with another tensor.
+
+    This function reshapes the frequency tensor to have the same shape as the target tensor 'x'
+    for the purpose of broadcasting the frequency tensor during element-wise operations.
+
+    The input freqs_cis tensor is assumed to be of shape (max_seqlen, dim),
+    and the first seqlen elements will be sliced, but dim must match x.
+
+    Args:
+        freqs_cis (torch.Tensor): Frequency tensor to be reshaped.
+        x (torch.Tensor): Target tensor for broadcasting compatibility.
+
+    Returns:
+        torch.Tensor: Reshaped frequency tensor.
+    """
+    ndim = x.ndim
+    assert ndim > 1
+    seqlen = x.shape[1]
+    freqs_cis = freqs_cis[0:seqlen]
+    assert freqs_cis.shape == (seqlen, x.shape[-1])
+    shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
+    return freqs_cis.view(*shape)
+
+
+def apply_rotary_emb(
+    xq: torch.Tensor,
+    xk: torch.Tensor,
+    freqs_cis: torch.Tensor,
+) -> tuple[torch.Tensor, torch.Tensor]:
+    """
+    Apply rotary embeddings to input tensors using the given frequency tensor.
+
+    This function applies rotary embeddings to the given query 'xq' and key 'xk' tensors using the provided
+    frequency tensor 'freqs_cis'. The input tensors are reshaped as complex numbers, and the frequency tensor
+    is reshaped for broadcasting compatibility. The resulting tensors contain rotary embeddings and are
+    returned as real tensors.
+
+    Args:
+        xq (torch.Tensor): Query tensor to apply rotary embeddings.
+        xk (torch.Tensor): Key tensor to apply rotary embeddings.
+        freqs_cis (torch.Tensor): Precomputed frequency tensor for complex exponentials.
+
+    Returns:
+        tuple[torch.Tensor, torch.Tensor]: Tuple of modified query tensor and key tensor with rotary embeddings.
+    """
+    xq_ = torch.view_as_complex(xq.float().reshape(*xq.shape[:-1], -1, 2))
+    xk_ = torch.view_as_complex(xk.float().reshape(*xk.shape[:-1], -1, 2))
+    freqs_cis = reshape_for_broadcast(freqs_cis, xq_)
+    xq_out = torch.view_as_real(xq_ * freqs_cis).flatten(3)
+    xk_out = torch.view_as_real(xk_ * freqs_cis).flatten(3)
+    return xq_out.type_as(xq), xk_out.type_as(xk)
+
+
+def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
+    """torch.repeat_interleave(x, dim=2, repeats=n_rep)"""
+    bs, slen, n_kv_heads, head_dim = x.shape
+    if n_rep == 1:
+        return x
+    return (
+        torch.unsqueeze(x, dim=3)
+        .expand(bs, slen, n_kv_heads, n_rep, head_dim)
+        .reshape(bs, slen, n_kv_heads * n_rep, head_dim)
+    )
+
+
+class Attention(nn.Module):
+    """
+    Multi-head attention module.
+
+    Args:
+        model_args (TransformerModelArgs): Model configuration arguments.
+
+    Attributes:
+        n_kv_heads (int): Number of key and value heads.
+        n_heads (int): Number of query heads.
+        n_rep (int): Number of repetitions for local heads.
+        head_dim (int): Dimension size of each attention head.
+        wq (Linear): Linear transformation for queries.
+        wk (Linear): Linear transformation for keys.
+        wv (Linear): Linear transformation for values.
+        wo (Linear): Linear transformation for output.
+
+    """
+
+    def __init__(self, model_args):
+        super().__init__()
+        self.n_heads = model_args.n_heads
+        self.n_kv_heads = (
+            model_args.n_heads
+            if model_args.n_kv_heads is None
+            else model_args.n_kv_heads
+        )
+        self.n_rep = self.n_heads // self.n_kv_heads
+        self.head_dim = model_args.dim // model_args.n_heads
+
+        self.wq = nn.Linear(
+            model_args.dim, model_args.n_heads * self.head_dim, bias=False
+        )
+        self.wk = nn.Linear(model_args.dim, self.n_kv_heads * self.head_dim, bias=False)
+        self.wv = nn.Linear(model_args.dim, self.n_kv_heads * self.head_dim, bias=False)
+        self.wo = nn.Linear(
+            model_args.n_heads * self.head_dim, model_args.dim, bias=False
+        )
+
+    def forward(
+        self,
+        x: torch.Tensor,
+        freqs_cis: torch.Tensor,
+        layer_id: int,
+        kv_cache: list,
+        curr_idx: int,
+    ):
+        """
+        Forward pass of the attention module.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+            freqs_cis (torch.Tensor): Precomputed frequency tensor.
+
+        Returns:
+            torch.Tensor: Output tensor after attention.
+
+        """
+
+        bs, seqlen, _ = x.shape
+        xq, xk, xv = self.wq(x), self.wk(x), self.wv(x)
+
+        # Use -1 instead of `n_heads` (or `n_kv_heads`) to infer the actual
+        # local heads from sizes of xq, xk, and xv as TP may have sharded them
+        # after the above linear ops.
+        xq = xq.view(bs, seqlen, -1, self.head_dim)
+        xk = xk.view(bs, seqlen, -1, self.head_dim)
+        xv = xv.view(bs, seqlen, -1, self.head_dim)
+
+        curr_freqs_cis = freqs_cis[curr_idx: curr_idx + x.size(1)]
+        xq, xk = apply_rotary_emb(xq, xk, freqs_cis=curr_freqs_cis)
+
+        # # repeat k/v heads if n_kv_heads < n_heads
+        # keys = repeat_kv(xk, self.n_rep)  # (bs, seqlen, n_local_heads, head_dim)
+        # values = repeat_kv(xv, self.n_rep)  # (bs, seqlen, n_local_heads, head_dim)
+
+        # xq = xq.transpose(1, 2)  # (bs, n_local_heads, seqlen, head_dim)
+        # xk = keys.transpose(1, 2)  # (bs, n_local_heads, seqlen, head_dim)
+        # xv = values.transpose(1, 2)  # (bs, n_local_heads, seqlen, head_dim)
+
+        # output = F.scaled_dot_product_attention(
+        #     xq, xk, xv, attn_mask=None, dropout_p=0.0, is_causal=True
+        # )
+
+        # output = output.transpose(
+        #     1, 2
+        # ).contiguous()  # (bs, seqlen, n_local_heads, head_dim)
+
+        assert kv_cache is not None
+        k_cache, v_cache = kv_cache[int(layer_id)]
+        output = flash_attn_with_kvcache(
+            xq, k_cache, v_cache,
+            k=xk, v=xv,
+            cache_seqlens=curr_idx,
+            causal=True,
+        )
+        output = output.view(bs, seqlen, -1)
+        return self.wo(output)
+
+
+class FeedForward(nn.Module):
+    """
+    FeedForward module
+
+    Args:
+        dim (int): Input dimension.
+        hidden_dim (int): Hidden dimension of the feedforward layer.
+        multiple_of (int): Value to ensure hidden dimension is a multiple of this value.
+        ffn_dim_multiplier (float | None): Custom multiplier for hidden dimension. Defaults to None.
+
+    Attributes:
+        w1 (Linear): Linear transformation for the first layer.
+        w2 (Linear): Linear transformation for the second layer.
+        w3 (Linear): Linear transformation for the third layer.
+
+    """
+
+    def __init__(
+        self,
+        dim: int,
+        hidden_dim: int,
+        multiple_of: int,
+        ffn_dim_multiplier: float | None,
+    ):
+        super().__init__()
+        hidden_dim = int(2 * hidden_dim / 3)
+        # custom dim factor multiplier
+        if ffn_dim_multiplier is not None:
+            hidden_dim = int(ffn_dim_multiplier * hidden_dim)
+        hidden_dim = multiple_of * ((hidden_dim + multiple_of - 1) // multiple_of)
+
+        self.w1 = nn.Linear(dim, hidden_dim, bias=False)
+        self.w2 = nn.Linear(hidden_dim, dim, bias=False)
+        self.w3 = nn.Linear(dim, hidden_dim, bias=False)
+
+    def forward(self, x):
+        return self.w2(F.silu(self.w1(x)) * self.w3(x))
+
+
+class TransformerBlock(nn.Module):
+    """
+    TransformerBlock Module
+
+    Args:
+        layer_id (int): Identifier for the layer.
+        model_args (TransformerModelArgs): Model configuration arguments.
+
+    Attributes:
+        n_heads (int): Number of attention heads.
+        dim (int): Dimension size of the model.
+        head_dim (int): Dimension size of each attention head.
+        attention (Attention): Attention module.
+        feed_forward (FeedForward): FeedForward module.
+        layer_id (int): Identifier for the layer.
+        attention_norm (RMSNorm): Layer normalization for attention output.
+        ffn_norm (RMSNorm): Layer normalization for feedforward output.
+
+    """
+
+    def __init__(self, layer_id: int, model_args):
+        super().__init__()
+        self.n_heads = model_args.n_heads
+        self.dim = model_args.dim
+        self.attention = Attention(model_args)
+        self.feed_forward = FeedForward(
+            dim=model_args.dim,
+            hidden_dim=4 * model_args.dim,
+            multiple_of=model_args.multiple_of,
+            ffn_dim_multiplier=model_args.ffn_dim_multiplier,
+        )
+        self.attention_norm = nn.RMSNorm(model_args.dim, eps=model_args.norm_eps)
+        self.ffn_norm = nn.RMSNorm(model_args.dim, eps=model_args.norm_eps)
+
+    def forward(
+        self,
+        x: torch.Tensor,
+        freqs_cis: torch.Tensor,
+        layer_id: int,
+        kv_cache: list,
+        curr_idx: int,
+    ):
+        """
+        Perform a forward pass through the TransformerBlock.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+            freqs_cis (torch.Tensor): Precomputed cosine and sine frequencies.
+
+        Returns:
+            torch.Tensor: Output tensor after applying attention and feedforward layers.
+
+        """
+        h = x + self.attention(self.attention_norm(x), freqs_cis=freqs_cis, layer_id=layer_id, kv_cache=kv_cache, curr_idx=curr_idx)
+        out = h + self.feed_forward(self.ffn_norm(h))
+        return out
+
+
+class Transformer(nn.Module):
+    """
+    Transformer Module
+
+    Args:
+        model_args (TransformerModelArgs): Model configuration arguments.
+
+    Attributes:
+        model_args (TransformerModelArgs): Model configuration arguments.
+        vocab_size (int): Vocabulary size.
+        n_layers (int): Number of layers in the model.
+        tok_embeddings (ParallelEmbedding): Token embeddings.
+        layers (torch.nn.ModuleList): List of Transformer blocks.
+        norm (RMSNorm): Layer normalization for the model output.
+        output (Linear): Linear layer for final output.
+        freqs_cis (torch.Tensor): Precomputed cosine and sine frequencies.
+
+    """
+
+    def __init__(self, model_args):
+        super().__init__()
+        self.model_args = model_args
+        self.vocab_size = model_args.vocab_size
+        self.n_layers = model_args.n_layers
+        self.tok_embeddings = nn.Embedding(model_args.vocab_size, model_args.dim)
+
+        # TODO persistent should be set to false, since this buffer can be recomputed.
+        # however, we set it to true for 2 reasons.  (1) due to pytorch/pytorch#123411,
+        # compile or pipeline-tracer will not correctly handle non-persistent buffers,
+        # so we need to fix that.  (2) if we initialize pipeline-parallel models from
+        # a seed checkpoint rather than calling init_weights, we need freqs_cis to be
+        # initialized by the checkpoint, or we need to add a separate initializer for
+        # just the non-persistent buffers that is called after loading checkpoints.
+        self.register_buffer("freqs_cis", self._precompute_freqs_cis(), persistent=False)
+
+        self.layers = torch.nn.ModuleDict()
+        for layer_id in range(model_args.n_layers):
+            self.layers[str(layer_id)] = TransformerBlock(layer_id, model_args)
+        self.norm = nn.RMSNorm(model_args.dim, eps=model_args.norm_eps)
+        self.output = nn.Linear(model_args.dim, model_args.vocab_size, bias=False)
+
+    def _precompute_freqs_cis(self) -> torch.Tensor:
+        return precompute_freqs_cis(
+            self.model_args.dim // self.model_args.n_heads,
+            # Need to compute until at least the max token limit for generation
+            # TODO: explain in docs/composability.md why we removed the 2x
+            # relaxing in our CP enablement PR
+            self.model_args.max_seq_len,
+            self.model_args.rope_theta,
+        )
+
+    def forward(
+        self,
+        tokens: torch.Tensor,
+        kv_cache: list,
+        curr_idx: int,
+        eos_id: int | None = None,
+        input_batch: torch.Tensor | None = None,
+    ):
+        """
+        Perform a forward pass through the Transformer model.
+
+        Args:
+            tokens (torch.Tensor): Input token indices if pipeline parallelism is not enabled.
+                If pipeline parallelism is enabled, this will be the input token indices
+                for the ranks on the first pipeline stage. This will be the activation of the
+                previous pipeline stage if the current rank is not on the first stage.
+            input_batch (torch.Tensor): The input batch read from the dataloader.
+                This will always be the input batch regardless of the pipeline stage.
+                This field is required for non-first PP stages to perform document
+                masking attention (to analyze the boundary of the document).
+
+        Returns:
+            torch.Tensor: Output logits after applying the Transformer model.
+
+        """
+        # passthrough for nonexistent layers, allows easy configuration of pipeline parallel stages
+        h = self.tok_embeddings(tokens) if self.tok_embeddings else tokens
+
+        for layer_id, layer in self.layers.items():
+            h = layer(h, freqs_cis=self.freqs_cis, layer_id=layer_id, kv_cache=kv_cache, curr_idx=curr_idx)
+
+        h = self.norm(h) if self.norm else h
+        output = self.output(h) if self.output else h
+        return output
