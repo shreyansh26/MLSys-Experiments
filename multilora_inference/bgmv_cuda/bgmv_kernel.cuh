f2fea8a00d2bc983a4dc356bb637c79fa9657718
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-09-19 21:56:25 -0700

    Add shrink kernel for bgmv

---
+++ multilora_inference/bgmv_cuda/bgmv_kernel.cuh
@@ -0,0 +1,189 @@
+#pragma once
+
+#include <cooperative_groups.h>
+#include <cuda_runtime.h>
+
+#include <cuda/pipeline>
+#include <iostream>
+#include <cassert>
+#include <stdexcept>
+
+#include <cub/util_type.cuh>
+
+namespace cg = cooperative_groups;
+
+__host__ __device__ inline int cdiv(int a, int b) {
+    return (a + b - 1) / b;
+}
+
+template <int F_in, int F_out, typename T>
+__global__ void bgmv_shrink_kernel(T* Y,
+                                   const T* X,
+                                   const T* W,
+                                   const int* indices,
+                                   const int num_layers,
+                                   const int layer_idx,
+                                   const T scale) {
+    auto block = cg::this_thread_block();
+
+    const int b = blockIdx.y;
+    const int j = blockIdx.x;
+
+    constexpr int vec_size = 16 / sizeof(T);
+    constexpr size_t tx = 32; // threadIdx.x is also 32
+    constexpr size_t ty = 4; // threadIdx.y is also 4
+    constexpr size_t tile_size = tx * ty * vec_size;
+    constexpr size_t num_pipeline_stages = 2;
+
+    using Vec = cub::CubVector<T, vec_size>;
+
+    __shared__ T W_shared[num_pipeline_stages * tile_size];
+    __shared__ T X_shared[num_pipeline_stages * tile_size];
+
+    __shared__ float y_warpwise[ty];
+
+    const int idx = indices[b] * num_layers + layer_idx;
+
+    size_t W_shared_offset[num_pipeline_stages] = {0U, 1U * tile_size};
+    size_t X_shared_offset[num_pipeline_stages] = {0U, 1U * tile_size};
+
+    auto pipe = cuda::make_pipeline();
+
+    // Pipeline the load of W and X with computation of WX
+    pipe.producer_acquire();
+    // tile_idx is 0 here so not included in the load indexing
+    cuda::memcpy_async(W_shared + (threadIdx.y * tx + threadIdx.x) * vec_size, 
+                       W + (idx * F_out + j) * F_in + (threadIdx.y * tx + threadIdx.x) * vec_size,
+                       cuda::aligned_size_t<16>(16), 
+                       pipe);
+    cuda::memcpy_async(X_shared + (threadIdx.y * tx + threadIdx.x) * vec_size,
+                       X + b * F_in + (threadIdx.y * tx + threadIdx.x) * vec_size,
+                       cuda::aligned_size_t<16>(16),
+                       pipe);
+    pipe.producer_commit();
+
+    size_t copy_buffer_idx, compute_idx;
+
+    float y = 0.f;
+    Vec x_vec, w_vec;
+    size_t tile_idx;
+
+#pragma unroll
+    for(tile_idx = 1; tile_idx < cdiv(F_in, tile_size); tile_idx++) {
+        copy_buffer_idx = tile_idx % num_pipeline_stages;
+        // pipeline stage: async copy W and X fragment
+        pipe.producer_acquire();
+
+        if(tile_idx * tile_size + (threadIdx.y * tx) * vec_size < F_in) {
+            cuda::memcpy_async(W_shared + W_shared_offset[copy_buffer_idx] + (threadIdx.y * tx + threadIdx.x) * vec_size, 
+                        W + (idx * F_out + j) * F_in + tile_idx * tile_size + (threadIdx.y * tx + threadIdx.x) * vec_size,
+                        cuda::aligned_size_t<16>(16), 
+                        pipe);
+            cuda::memcpy_async(X_shared + X_shared_offset[copy_buffer_idx] + (threadIdx.y * tx + threadIdx.x) * vec_size,
+                        X + b * F_in + tile_idx * tile_size + (threadIdx.y * tx + threadIdx.x) * vec_size,
+                        cuda::aligned_size_t<16>(16),
+                        pipe);
+        }
+        pipe.producer_commit();
+
+        compute_idx = (tile_idx - 1) % num_pipeline_stages;
+        // pipeline stage: compute WX
+        pipe.consumer_wait();
+        block.sync();
+
+        const T* x_ptr = X_shared + X_shared_offset[compute_idx] + (threadIdx.y * tx + threadIdx.x) * vec_size;
+        const T* w_ptr = W_shared + W_shared_offset[compute_idx] + (threadIdx.y * tx + threadIdx.x) * vec_size;
+
+        float sum = 0.f;
+
+#pragma unroll
+        for (int i = 0; i < vec_size; ++i) {
+            sum += float(w_ptr[i]) * float(x_ptr[i]) * float(scale);
+        }
+
+#pragma unroll
+        // intra-warp reduction
+        for(size_t offset = tx / 2; offset > 0; offset /= 2) {
+            sum += __shfl_down_sync(0xffffffff, sum, offset);
+        }
+        // stores warp wise sum (one lane per warp)
+        if (threadIdx.x == 0) {
+            y_warpwise[threadIdx.y] = sum;
+        }
+        block.sync();
+
+#pragma unroll
+        for(size_t i = 0; i < ty; i++) {
+            y += y_warpwise[i];
+        }
+
+        block.sync();
+        pipe.consumer_release();
+    }
+
+    // Last stage is only compute WX
+    compute_idx = (tile_idx - 1) % num_pipeline_stages;
+    // pipeline stage: compute WX
+    pipe.consumer_wait();
+    block.sync();
+
+    const T* x_ptr = X_shared + X_shared_offset[compute_idx] + (threadIdx.y * tx + threadIdx.x) * vec_size;
+    const T* w_ptr = W_shared + W_shared_offset[compute_idx] + (threadIdx.y * tx + threadIdx.x) * vec_size;
+
+    float sum = 0.f;
+
+#pragma unroll
+    for (int i = 0; i < vec_size; ++i) {
+        sum += float(w_ptr[i]) * float(x_ptr[i]) * float(scale);
+    }
+
+#pragma unroll
+    // intra-warp reduction
+    for(size_t offset = tx / 2; offset > 0; offset /= 2) {
+        sum += __shfl_down_sync(0xffffffff, sum, offset);
+    }
+    // stores warp wise sum - but check bounds since it might be out of bounds
+    if (threadIdx.x == 0) {
+        if ((tile_idx - 1) * tile_size + (threadIdx.y * tx) * vec_size < F_in) {
+            y_warpwise[threadIdx.y] = sum;
+        } else {
+            y_warpwise[threadIdx.y] = 0.f;
+        }
+    }
+    block.sync();
+
+#pragma unroll
+    for(size_t i = 0; i < ty; i++) {
+        y += y_warpwise[i];
+    }
+
+    block.sync();
+    pipe.consumer_release();
+
+    // write Y
+    if(block.thread_rank() == 0) {
+        Y[b * F_out + j] += y;
+    }   
+}
+
+
+template <int F_in, int F_out, typename T>
+void bgmv_kernel(T* Y,
+                 const T* X,
+                 const T* W,
+                 const int* indices,
+                 const int num_layers,
+                 const int layer_idx,
+                 const T scale,
+                 const int batch_size) {
+    if(F_in < F_out) {
+        throw std::runtime_error("F_in < F_out case not implemented yet");
+    }
+    else {
+        constexpr int vec_size = 16 / sizeof(T);
+        assert(F_in % (vec_size * 32) == 0);
+        dim3 grid(F_out, batch_size);
+        dim3 block(32, 4);
+        bgmv_shrink_kernel<F_in, F_out, T><<<grid, block>>>(Y, X, W, indices, num_layers, layer_idx, scale);
+    }
+}
