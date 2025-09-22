e906a48ef932d4bee8cdcad07aed56e67d339d43
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-09-21 21:21:11 -0700

    Add n > 1 sequence length support in triton kernel test script

--- multilora_inference/bgmv_triton/api.py
+++ multilora_inference/bgmv_triton/api.py
@@ -10,25 +10,24 @@ def lora_bgmv_triton(y: torch.Tensor,
                      layer_idx: int = 0,
                      scale: float = 1.0) -> torch.Tensor:
     assert y.is_cuda and x.is_cuda and A.is_cuda and B.is_cuda and I.is_cuda, "All tensors must be on CUDA"
-    assert y.dim() == 3 and y.size(1) == 1, "y must be [B, 1, out_dim]"
-    assert x.dim() == 3 and x.size(1) == 1, "x must be [B, 1, in_dim]"
+    assert y.dim() == 3, "y must be [B, n, out_dim]"
+    assert x.dim() == 3, "x must be [B, n, in_dim]"
     assert A.dim() == 3 and B.dim() == 3, "A [L*num_layers, r, in_dim], B [L*num_layers, out_dim, r]"
     assert I.dim() == 1 and I.size(0) == y.size(0), "I shape must match batch size"
 
-    batch_size = x.size(0)
-    in_dim = x.size(2)
-    out_dim = y.size(2)
+    Bsz, n, in_dim  = x.shape
+    By,  ny, out_dim = y.shape
+
+    assert Bsz == By and n == ny, "x/y batch or sequence length mismatch"
+
     rank = A.size(1)
     L = A.size(0)
 
     original_y_shape = y.shape
-    x = x.view(batch_size, in_dim)
-    y = y.view(batch_size, out_dim)
 
-    y_intermediate = torch.zeros(batch_size, rank, dtype=x.dtype, device=y.device)
+    y_intermediate = torch.zeros(Bsz, n, rank, dtype=x.dtype, device=x.device)
 
-    bgmv_triton(y_intermediate, x, A, I, num_layers=int(num_layers), layer_idx=int(layer_idx), scale=float(scale), accumulate=False)
-    bgmv_triton(y, y_intermediate, B, I, num_layers=int(num_layers), layer_idx=int(layer_idx), scale=float(scale), accumulate=True)
+    bgmv_triton(y_intermediate, x, A, I, num_layers=int(num_layers), layer_idx=int(layer_idx), scale=1.0, accumulate=False)
+    bgmv_triton(y, y_intermediate, B, I, num_layers=int(num_layers), layer_idx=int(layer_idx), scale=float(scale), accumulate=True) # Apply scale only in final matmul
 
-    # Return reshaped view
-    return y.view(original_y_shape)
+    return y
--- multilora_inference/bgmv_triton/run_bgmv.py
+++ multilora_inference/bgmv_triton/run_bgmv.py
@@ -8,10 +8,13 @@ layer_idx = 2
 F_in = 16
 F_out = 1024
 scale = 0.25
+n = 32
 
 dtype = torch.float16  # or torch.bfloat16, torch.float32
 device = 'cuda'
 
+## 2D case
+print("2D case")
 X = torch.randn(B, F_in, device=device, dtype=dtype)
 W = torch.randn(L * num_layers, F_out, F_in, device=device, dtype=dtype)
 Y = torch.zeros(B, F_out, device=device, dtype=dtype)
@@ -24,6 +27,24 @@ idx = indices.to(torch.long) * num_layers + layer_idx
 # Gather W[idx] : shape [B, F_out, F_in]
 W_sel = W[idx, :, :]
 # Batched matmul: [B, F_out, F_in] x [B, F_in, 1] -> [B, F_out, 1]
-ref = torch.bmm(W_sel, X.unsqueeze(-1)).squeeze(-1) * scale  # [B, F_out]
+ref = torch.einsum("bfi,bi->bf", W_sel, X) * scale  # [B, F_out]
+max_abs_diff = (ref - Y).abs().max().item()
+print("Max abs diff:", max_abs_diff)
+
+## 3D case
+print("3D case")
+X = torch.randn(B, n, F_in, device=device, dtype=dtype)
+W = torch.randn(L * num_layers, F_out, F_in, device=device, dtype=dtype)
+Y = torch.zeros(B, n, F_out, device=device, dtype=dtype)
+indices = torch.randint(low=0, high=L, size=(B,), device=device, dtype=torch.int32)
+
+bgmv_triton(Y, X, W, indices, num_layers=num_layers, layer_idx=layer_idx, scale=scale)
+
+# Correctness check vs. PyTorch
+idx = indices.to(torch.long) * num_layers + layer_idx
+# Gather W[idx] : shape [B, F_out, F_in]
+W_sel = W[idx, :, :]
+# Batched matmul: [B, F_out, F_in] x [B, F_in, 1] -> [B, F_out, 1]
+ref = torch.einsum("bfi,bni->bnf", W_sel, X) * scale  # [B, F_out]
 max_abs_diff = (ref - Y).abs().max().item()
 print("Max abs diff:", max_abs_diff)
